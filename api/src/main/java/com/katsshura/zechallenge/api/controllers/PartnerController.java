e426eb0c04cc02f7d5c643fc38ec8917704341af
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2021-03-21 16:06:57 -0300

    implemented endpoint for searching a partner by id

--- api/src/main/java/com/katsshura/zechallenge/api/controllers/PartnerController.java
+++ api/src/main/java/com/katsshura/zechallenge/api/controllers/PartnerController.java
@@ -1,15 +1,13 @@
 package com.katsshura.zechallenge.api.controllers;
 
 import com.katsshura.ze.challenge.domain.interfaces.PartnerDataManagement;
+import com.katsshura.ze.challenge.domain.interfaces.PartnerServiceDefinition;
 import com.katsshura.ze.challenge.domain.models.GeoInformation;
 import com.katsshura.ze.challenge.domain.models.Partner;
 import com.katsshura.zechallenge.api.viewModels.PartnerModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,9 +17,11 @@ import java.util.stream.Collectors;
 public class PartnerController {
 
     private final PartnerDataManagement partnerManagement;
+    private final PartnerServiceDefinition partnerService;
 
-    public PartnerController(PartnerDataManagement partnerManagement) {
+    public PartnerController(PartnerDataManagement partnerManagement, PartnerServiceDefinition partnerService) {
         this.partnerManagement = partnerManagement;
+        this.partnerService = partnerService;
     }
 
     @RequestMapping(value = "/create/single", method = RequestMethod.POST)
@@ -36,15 +36,27 @@ public class PartnerController {
     }
 
 
-    @RequestMapping(value = "/create/multiple")
+    @RequestMapping(value = "/create/multiple", method = RequestMethod.POST)
     private ResponseEntity createPartner(@RequestBody List<PartnerModel> body) {
         try {
-            var partners = body.stream().map(model -> toPartner(model))
+            var partners = body.stream().map(this::toPartner)
                     .collect(Collectors.toList());
 
-            partners.forEach(partner -> partnerManagement.save(partner));
+            partners.forEach(partnerManagement::save);
 
             return new ResponseEntity("All partners saved with success", HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    private ResponseEntity findPartnerById(@PathVariable String id) {
+        try {
+            var partner = partnerManagement.find(id);
+            return partner != null
+                    ? new ResponseEntity(partner, HttpStatus.OK)
+                    : new ResponseEntity(HttpStatus.NO_CONTENT);
         }catch (Exception e) {
             return new ResponseEntity(e, HttpStatus.INTERNAL_SERVER_ERROR);
         }
