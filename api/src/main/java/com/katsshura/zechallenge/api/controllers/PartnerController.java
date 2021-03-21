b35fc91df4b6c123495e6755519500dc62509a80
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2021-03-21 17:24:55 -0300

    refactor: removed extra code and implemented conversion

--- api/src/main/java/com/katsshura/zechallenge/api/controllers/PartnerController.java
+++ api/src/main/java/com/katsshura/zechallenge/api/controllers/PartnerController.java
@@ -2,8 +2,7 @@ package com.katsshura.zechallenge.api.controllers;
 
 import com.katsshura.ze.challenge.domain.interfaces.PartnerDataManagement;
 import com.katsshura.ze.challenge.domain.interfaces.PartnerServiceDefinition;
-import com.katsshura.ze.challenge.domain.models.geographical.GeoInformation;
-import com.katsshura.ze.challenge.domain.models.Partner;
+import com.katsshura.zechallenge.api.util.ViewModelConversion;
 import com.katsshura.zechallenge.api.viewModels.PartnerModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,16 +17,18 @@ public class PartnerController {
 
     private final PartnerDataManagement partnerManagement;
     private final PartnerServiceDefinition partnerService;
+    private final ViewModelConversion conversion;
 
-    public PartnerController(PartnerDataManagement partnerManagement, PartnerServiceDefinition partnerService) {
+    public PartnerController(PartnerDataManagement partnerManagement, PartnerServiceDefinition partnerService, ViewModelConversion conversion) {
         this.partnerManagement = partnerManagement;
         this.partnerService = partnerService;
+        this.conversion = conversion;
     }
 
     @RequestMapping(value = "/create/single", method = RequestMethod.POST)
     private ResponseEntity createPartner(@RequestBody PartnerModel body) {
         try {
-            var partner = toPartner(body);
+            var partner = conversion.toDomainPartner(body);
             var result = partnerManagement.save(partner);
             return new ResponseEntity("Partner saved with success", HttpStatus.OK);
         }catch (Exception e) {
@@ -39,7 +40,7 @@ public class PartnerController {
     @RequestMapping(value = "/create/multiple", method = RequestMethod.POST)
     private ResponseEntity createPartner(@RequestBody List<PartnerModel> body) {
         try {
-            var partners = body.stream().map(this::toPartner)
+            var partners = body.stream().map(conversion::toDomainPartner)
                     .collect(Collectors.toList());
 
             partners.forEach(partnerManagement::save);
@@ -55,23 +56,10 @@ public class PartnerController {
         try {
             var partner = partnerManagement.find(id);
             return partner != null
-                    ? new ResponseEntity(partner, HttpStatus.OK)
+                    ? new ResponseEntity(conversion.toViewModelPartner(partner), HttpStatus.OK)
                     : new ResponseEntity(HttpStatus.NO_CONTENT);
         }catch (Exception e) {
             return new ResponseEntity(e, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-    }
-
-    private Partner toPartner(PartnerModel model) {
-        var coverageArea = new GeoInformation<>(model.getCoverageArea().getType(), model.getCoverageArea().getCoordinates());
-        var address = new GeoInformation<>(model.getAddress().getType(), model.getAddress().getCoordinates());
-        return new Partner(
-                model.getId(),
-                model.getTradingName(),
-                model.getOwnerName(),
-                model.getDocument(),
-                coverageArea,
-                address
-        );
     }
 }
