fd4e62e9ffe550da5a80220fb5278a073926b59b
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2021-03-22 01:20:38 -0300

    refactor: added geographical definitions to interfaces

--- domain/src/main/java/com/katsshura/ze/challenge/domain/interfaces/PartnerServiceDefinition.java
+++ domain/src/main/java/com/katsshura/ze/challenge/domain/interfaces/PartnerServiceDefinition.java
@@ -1,7 +1,8 @@
 package com.katsshura.ze.challenge.domain.interfaces;
 
 import com.katsshura.ze.challenge.domain.models.Partner;
+import com.katsshura.ze.challenge.domain.models.geographical.Coordinate;
 
 public interface PartnerServiceDefinition {
-    Partner getNearestPartnerBasedOnLocation(double longitude, double latitude);
+    Partner getNearestPartnerBasedOnLocation(Coordinate actual);
 }
