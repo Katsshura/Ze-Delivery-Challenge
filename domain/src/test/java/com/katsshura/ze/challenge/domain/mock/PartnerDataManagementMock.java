f05b846e848eebab220ede04321164087694c63c
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2021-03-22 21:01:01 -0300

    Included mocked data for tests

--- domain/src/test/java/com/katsshura/ze/challenge/domain/mock/PartnerDataManagementMock.java
+++ domain/src/test/java/com/katsshura/ze/challenge/domain/mock/PartnerDataManagementMock.java
@@ -1,14 +1,10 @@
 package com.katsshura.ze.challenge.domain.mock;
 
 import com.katsshura.ze.challenge.domain.interfaces.PartnerDataManagement;
-import com.katsshura.ze.challenge.domain.models.geographical.Coordinate;
-import com.katsshura.ze.challenge.domain.models.geographical.GeoInformation;
+import com.katsshura.ze.challenge.domain.models.geographical.*;
 import com.katsshura.ze.challenge.domain.models.Partner;
-import com.katsshura.ze.challenge.domain.models.geographical.GeoMultiPolygon;
-import com.katsshura.ze.challenge.domain.models.geographical.GeoPolygon;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class PartnerDataManagementMock implements PartnerDataManagement {
     @Override
@@ -18,7 +14,7 @@ public class PartnerDataManagementMock implements PartnerDataManagement {
 
     @Override
     public Partner find(String id) {
-        return null;
+        return partners().stream().filter(partner -> partner.getId().equals(id)).findFirst().orElse(null);
     }
 
     @Override
@@ -31,68 +27,129 @@ public class PartnerDataManagementMock implements PartnerDataManagement {
         return false;
     }
 
+    @Override
+    public Set<GeographicalPartner> findAllGeographicalPartner() {
+        var partners = Arrays.asList(
+                new GeographicalPartner(
+                        "1",
+                        new Coordinate(20, 40),
+                        new GeoMultiPolygon(
+                                Collections.singletonList(
+                                        new GeoPolygon(
+                                                getAllCoordinatesFromPolygon(
+                                                    Arrays.asList(
+                                                            new Coordinate(-50, -20),
+                                                            new Coordinate(-50, 30),
+                                                            new Coordinate(-30, 50),
+                                                            new Coordinate(40, 50),
+                                                            new Coordinate(40, 20),
+                                                            new Coordinate(20, -20)
+                                                    )
+                                                )
+                                        )
+                                )
+                        )
+                ),
+                new GeographicalPartner(
+                        "2",
+                        new Coordinate(40, -20),
+                        new GeoMultiPolygon(
+                                Collections.singletonList(
+                                        new GeoPolygon(
+                                                getAllCoordinatesFromPolygon(
+                                                    Arrays.asList(
+                                                            new Coordinate(-20, 20),
+                                                            new Coordinate(-20, -60),
+                                                            new Coordinate(60, -60),
+                                                            new Coordinate(60, 20)
+                                                    )
+                                                )
+                                        )
+                                )
+                        )
+                )
+        );
+        return new HashSet<>(partners);
+    }
+
     private List<Partner> partners() {
         return Arrays.asList(
                 new Partner(
                         "1",
-                        "Adega da Cerveja - Pinheiros",
-                        "Zé da Silva",
+                        "P1",
+                        "Teste 1",
                         "1432132123891/0001",
                         new GeoInformation<>(
                                 new GeoMultiPolygon(
-                                        Arrays.asList(
+                                        Collections.singletonList(
                                                 new GeoPolygon(
                                                         Arrays.asList(
-                                                                new Coordinate(30, 20),
-                                                                new Coordinate(45, 40),
-                                                                new Coordinate(10, 40),
-                                                                new Coordinate(30, 20)
-                                                        )
-                                                ),
-                                                new GeoPolygon(
-                                                        Arrays.asList(
-                                                                new Coordinate(15, 5),
-                                                                new Coordinate(40, 10),
-                                                                new Coordinate(10, 20),
-                                                                new Coordinate(5, 10),
-                                                                new Coordinate(15, 5)
+                                                                new Coordinate(-50, -20),
+                                                                new Coordinate(-50, 30),
+                                                                new Coordinate(-30, 50),
+                                                                new Coordinate(40, 50),
+                                                                new Coordinate(40, 20),
+                                                                new Coordinate(20, -20)
                                                         )
                                                 )
                                         )
                                 )
                         ),
-                        new GeoInformation<>(new Coordinate(-46.57421, -21.785741))
+                        new GeoInformation<>(new Coordinate(20, 40))
                 ),
                 new Partner(
-                        "1",
-                        "Adega da Cerveja - Pinheiros",
-                        "Zé da Silva",
-                        "1432132123891/0001",
+                        "2",
+                        "P2",
+                        "Teste 2",
+                        "1432132123891/0002",
                         new GeoInformation<>(
                                 new GeoMultiPolygon(
-                                        Arrays.asList(
+                                        Collections.singletonList(
                                                 new GeoPolygon(
                                                         Arrays.asList(
-                                                                new Coordinate(30, 20),
-                                                                new Coordinate(45, 40),
-                                                                new Coordinate(10, 40),
-                                                                new Coordinate(30, 20)
-                                                        )
-                                                ),
-                                                new GeoPolygon(
-                                                        Arrays.asList(
-                                                                new Coordinate(15, 5),
-                                                                new Coordinate(40, 10),
-                                                                new Coordinate(10, 20),
-                                                                new Coordinate(5, 10),
-                                                                new Coordinate(15, 5)
+                                                                new Coordinate(-20, 20),
+                                                                new Coordinate(-20, -60),
+                                                                new Coordinate(60, -60),
+                                                                new Coordinate(60, 20)
                                                         )
                                                 )
                                         )
                                 )
                         ),
-                        new GeoInformation<>(new Coordinate(-46.57421, -21.785741))
+                        new GeoInformation<>(new Coordinate(40, -20))
                 )
         );
+    }
+
+    private List<Coordinate> getAllCoordinatesFromPolygon(List<Coordinate> verticesCoordinates) {
+        var allCoordinates = new ArrayList<Coordinate>();
+
+        for (int i = 0; i < verticesCoordinates.size(); i++) {
+            var vertice = verticesCoordinates.get(i);
+            var nextVertice = i == verticesCoordinates.size()-1 ? verticesCoordinates.get(0) : verticesCoordinates.get(i + 1);
+
+            var x = vertice.getLongitude();
+            var y = vertice.getLatitude();
+
+            while (x != nextVertice.getLongitude() || y != nextVertice.getLatitude()) {
+                if(x < nextVertice.getLongitude()) {
+                    x += 1;
+                }else if (x > nextVertice.getLongitude()){
+                    x -= 1;
+                }
+
+                if(y < nextVertice.getLatitude()) {
+                    y += 1;
+                }else if(y > nextVertice.getLatitude()) {
+                    y -= 1;
+                }
+
+                var coordinate = new Coordinate(x, y);
+
+                allCoordinates.add(coordinate);
+            }
+        }
+
+        return allCoordinates;
     }
 }
